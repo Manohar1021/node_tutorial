849199f6ace62c48d0b1c2c3989d60c0970ed7c3
Manohar1021 <138129092+Manohar1021@users.noreply.github.com>
2023-09-29 10:55:02 +0530

    Update 3-modules.js

--- nodejs/3-modules.js
+++ nodejs/3-modules.js
@@ -5,6 +5,7 @@
 
 const names=require('./4-names')       //here calledby file name with ./ followed by file name
 const sayHi=require('./5-utils')       //here calledby file name with ./ followed by file name
+require('./6-mind-gernad')
 sayHi('man')
 sayHi(names.a)
 sayHi(names.b)
